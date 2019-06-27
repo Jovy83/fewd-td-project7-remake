889045114954e30a36420074e0d3dcee78017218
Jovy Ong <jovy83@live.com>
2019-06-27 09:11:36 +0800

    refactor previous ChartJS code

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ alert.html(`
 alert.on('click', (event) => {
     //todo: continue here
     const element = event.target;
-    if(element.classList.contains("alert-banner-close")) {
+    if (element.classList.contains("alert-banner-close")) {
         alert.hide();
     }
 });
@@ -20,69 +20,97 @@ const trafficChartCanvas = $('#traffic-chart');
 const dailyChartCanvas = $('#daily-chart');
 const mobileUsersChartCanvas = $('#mobile-users-chart');
 
-const trafficChart = new Chart(trafficChartCanvas, {
-    type: 'line',
-    data: {
-        labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
-        datasets: [{
-            data: [750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2250, 1750, 2250],
-            backgroundColor: 'rgba(226, 227, 244, 0.5)',
-            borderColor: 'rgba(115, 121, 186, 1)',
-            pointBackgroundColor: '#fff',
-            borderWidth: 1,
-            lineTension: 0
+// Traffic chart
+const trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+        "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+        backgroundColor: 'rgba(116, 119, 191, .3)',
+        borderWidth: 1,
+        lineTension: 0
+    }]
+};
+
+const trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+        duration: 0 // to make chart more responsive
+    },
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true
+            }
         }]
     },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        },
-        legend: {
-            display: false
-        },
+    legend: {
+        display: false
     }
+};
+
+const trafficChart = new Chart(trafficChartCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
 });
+
+// Daily chart
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+        label: '# of Hits',
+        data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: '#7477BF',
+        borderWidth: 1
+    }]
+};
+const dailyOptions = {
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true
+            }
+        }]
+    },
+    legend: {
+        display: false
+    }
+};
 
 const dailyChart = new Chart(dailyChartCanvas, {
     type: 'bar',
-    data: {
-        labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-        datasets: [{
-            data: [75, 100, 175, 125, 225, 200, 100],
-            backgroundColor: 'rgba(104, 108, 183, 1)',
-            borderColor: 'rgba(115, 121, 186, 1)',
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        },
-        legend: {
-            display: false
-        },
-    }
+    data: dailyData,
+    options: dailyOptions
 });
+
+// Mobile chart
+const mobileUsersData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+        label: '# of Users',
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            '#7477BF',
+            '#78CF82',
+            '#51B6C8'
+        ]
+    }]
+};
+
+const mobileOptions = {
+    legend: {
+        position: 'right',
+        labels: {
+            boxWidth: 20,
+            fontStyle: 'bold'
+        }
+    }
+}
 
 const mobileUsersChart = new Chart(mobileUsersChartCanvas, {
     type: 'doughnut',
-    data: {
-        labels: ['Phone', 'Tablet', 'Desktop'],
-        datasets: [{
-            data: [25, 10, 65],
-            backgroundColor: ['rgba(103, 168, 183, 1)', 'rgba(118, 194, 132, 1)', 'rgba(104, 108, 183, 1)'],
-            borderWidth: 1
-        }]
-    },
-    options: {
-
-    }
+    data: mobileUsersData,
+    options: mobileOptions
 }); 
